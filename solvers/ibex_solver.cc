6d52f0579c7c2a3815efaee5bf718c9080d1ddf9
Soonho Kong <soonho.kong@tri.global>
2021-06-24 15:04:10 -0400

    cc

--- solvers/ibex_solver.cc
+++ solvers/ibex_solver.cc
@@ -23,19 +23,18 @@ using Eigen::VectorXd;
 
 bool IbexSolver::is_available() { return true; }
 
-void IbexSolver::DoSolve(
-    const MathematicalProgram& prog,
-    const Eigen::VectorXd& initial_guess,
-    const SolverOptions& merged_options,
-    MathematicalProgramResult* result) const {
+void IbexSolver::DoSolve(const MathematicalProgram& prog,
+                         const Eigen::VectorXd& initial_guess,
+                         const SolverOptions& merged_options,
+                         MathematicalProgramResult* result) const {
   if (!prog.GetVariableScaling().empty()) {
     static const logging::Warn log_once(
-      "IbexSolver doesn't support the feature of variable scaling.");
+        "IbexSolver doesn't support the feature of variable scaling.");
   }
 
   unused(initial_guess);
   unused(merged_options);
-  
+
   ibex::Variable ibex_vars(prog.num_vars(), "x");
   ibex::SystemFactory factory;
   ibex::IntervalVector bounds(prog.num_vars());
@@ -45,20 +44,35 @@ void IbexSolver::DoSolve(
     const auto& ub = c->upper_bound();
 
     for (int k = 0; k < static_cast<int>(b.GetNumElements()); ++k) {
-      const size_t index =
-          prog.FindDecisionVariableIndex(b.variables()[k]);
+      const size_t index = prog.FindDecisionVariableIndex(b.variables()[k]);
       bounds[index] &= ibex::Interval(lb[k], ub[k]);
     }
   }
   factory.add_var(ibex_vars, bounds);
 
-  for (const auto&b : prog.linear_constraints()) {
+  for (const auto& b : prog.linear_constraints()) {
     symbolic::Formula f{b.evaluator()->CheckSatisfied(b.variables())};
-
   }
 
+  // Build SystemFactory. Add variables and constraints.
+  system_factory_ = make_unique<ibex::SystemFactory>();
+  system_factory_->add_var(ibex_converter_.variables());
+  for (const Formula& f : formulas_) {
+    if (!is_forall(f)) {
+      unique_ptr<const ibex::ExprCtr, ExprCtrDeleter> expr_ctr{
+          ibex_converter_.Convert(f)};
+      if (expr_ctr) {
+        system_factory_->add_ctr(*expr_ctr);
+        // We need to postpone the destruction of expr_ctr as it is
+        // still used inside of system_factory_.
+        expr_ctrs_.push_back(std::move(expr_ctr));
+      }
+    }
+  }
+  ibex_converter_.set_need_to_delete_variables(true);
+
   ibex::System sys(factory);
-  ibex::DefaultOptimizer o(sys, 1e-07); // TODO: take this as a parameter.
+  ibex::DefaultOptimizer o(sys, 1e-07);  // TODO: take this as a parameter.
   o.optimize(sys.box);
 }
 
